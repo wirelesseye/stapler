0435f75b650e663ac139efe0320662be1b8cc765
wirelessEye <okuloj@outlook.com>
2023-09-01 18:29:46 +1000

    Fix int type format

--- src/ast/types/int_type.rs
+++ src/ast/types/int_type.rs
@@ -2,7 +2,6 @@ use std::{fmt::Debug, any::Any};
 
 use super::{TypeTrait, TypeKind};
 
-#[derive(Debug)]
 pub enum IntType {
     I8,
     I32,
@@ -16,5 +15,15 @@ impl TypeTrait for IntType {
 
     fn as_any(&self) -> &dyn Any {
         self
+    }
+}
+
+impl Debug for IntType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::I8 => write!(f, "i8"),
+            Self::I32 => write!(f, "i32"),
+            Self::I64 => write!(f, "i64"),
+        }
     }
 }
